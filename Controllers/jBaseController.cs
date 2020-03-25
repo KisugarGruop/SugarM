1a5d9dd8c9bffb29834c444c1f4331f0255dc9e1
Thakorn-yo <thakorn@gmail.com>
2020-03-25 10:48:51 +0700

    Plantmap 25/3/2563

---
+++ Controllers/jBaseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SugarM.Controllers
+{
+    public class jBaseController : Controller
+    {
+        string getkey;
+        string url = "http://192.168.10.46/sdapi/sdapi/";
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult getTK()
+        {
+            getkey = Request.Cookies["Authorization"];
+            return Json(new { xtk = getkey, xurl = url });
+        }
+
+    }
+}
